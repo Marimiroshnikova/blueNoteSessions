2a92ead30b5278547134becccb32591ed99d1ae1
mari_miroshnikova <marina.miroshnikova.1@btu.edu.ge>
2025-12-27 23:31:21 +0400

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import noteIcon from '/musical-note.png';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="brand-wrapper">
         <div className="brand-icon">
-          <img src="/musical-note.png" alt="Musical note" />
+        <img src={noteIcon} alt="Musical note" />
         </div>
         <NavLink to="/" className="brand">
           Blue Note Sessions
